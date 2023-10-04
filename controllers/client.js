a9629682b40df57b2a82286bb26778b945ba1976
PETER MUYOMBA <111797748+pmuyomba11@users.noreply.github.com>
2023-10-03 20:21:17 -0400

    finished project

--- controllers/client.js
+++ controllers/client.js
@@ -31,6 +31,17 @@ router.delete('/:id', (req, res) => {
         res.status(500).json({error:'Error deleting client'})
     })
 })
+
+//Update route..
+router.put('/:id', (req, res) => {
+    Client.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .then((clientToBeUpdated) => {
+        res.redirect(`/clients/${clientToBeUpdated._id}`)
+    })
+    .catch((error) => {
+        res.status(501).send(err.message)
+    })
+})
 //create router
 router.post('/', (req, res) => {
    Client.create(req.body)
@@ -40,6 +51,19 @@ router.post('/', (req, res) => {
    .catch((err) => {
     res.status(501).send(err.message)
    })
+})
+
+//Edit route 
+router.get('/:id/edit', (req, res) => {
+    Client.findById(req.params.id)
+    .then((toBeEditted) => {
+        res.render('edit.ejs', {
+            clientTobeEdited : toBeEditted
+        })
+    })
+    .catch((err) => {
+        res.status(403).send("There is no such Client")
+    })
 })
 //Show route
 router.get('/:id', (req, res) => {
